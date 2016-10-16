5f69c8c425c48f9ad2848b46d5bca2717ba11d2e
ssdharma <ssdharma@ncsu.edu>
2016-10-16 19:44:02 -0400

    adding selenium folder

---
+++ Selenium/SlackSelenium.java
@@ -0,0 +1,85 @@
+package selenium.tests;
+
+import static org.junit.Assert.*;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.AfterClass;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.StaleElementReferenceException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import io.github.bonigarcia.wdm.ChromeDriverManager;
+
+public class SlackSelenium {
+	
+	private static WebDriver driver;
+
+	@BeforeClass
+	public static void setUp() throws Exception {
+		// driver = new HtmlUnitDriver();
+		ChromeDriverManager.getInstance().setup();
+		driver = new ChromeDriver();
+	}
+
+	@AfterClass
+	public static void tearDown() throws Exception {
+		driver.close();
+		driver.quit();
+	}
+
+	
+	//Selenium Base Test
+	@Test
+	public void postMessage()
+	{
+		driver.get("https://csc510-fall16.slack.com/");
+
+		// Wait until page loads and we can see a sign in button.
+		WebDriverWait wait = new WebDriverWait(driver, 30);
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("signin_btn")));
+
+		// Find email and password fields.
+		WebElement email = driver.findElement(By.id("email"));
+		WebElement pw = driver.findElement(By.id("password"));
+
+		// Type in our test user login info.
+		email.sendKeys("testUser@ncsu.edu");
+		pw.sendKeys("****");
+
+		// Click
+		WebElement signin = driver.findElement(By.id("signin_btn"));
+		signin.click();
+
+		// Wait until we go to general channel.
+		wait.until(ExpectedConditions.titleContains("general"));
+
+		// Switch to #bots channel and wait for it to load.
+		driver.get("https://csc510-fall16.slack.com/messages/bots");
+		wait.until(ExpectedConditions.titleContains("bots"));
+
+		// Type something
+		WebElement messageBot = driver.findElement(By.id("message-input"));
+		messageBot.sendKeys("hello world, from Selenium");
+		messageBot.sendKeys(Keys.RETURN);
+
+		wait.withTimeout(3, TimeUnit.SECONDS).ignoring(StaleElementReferenceException.class);
+
+		WebElement msg = driver.findElement(
+				By.xpath("//span[@class='message_body' and text() = 'hello world, from Selenium']"));
+		assertNotNull(msg);
+	}
+	
+	
+	
+}
