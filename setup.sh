9a11d37b53438e1dc90163b809044307b3ddaa72
Gaurav Aradhye <garadhye@redhat.com>
2016-10-26 22:20:43 -0400

    Modified setup.sh to install packages depending upon the OS type, moved the script related to starting the mongo server in separate file

--- setup.sh
+++ setup.sh
@@ -1,12 +1,35 @@
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
-echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
-sudo apt-get update
-sudo apt-get install -y mongodb-org
-echo "mongodb-org hold" | sudo dpkg --set-selections
-echo "mongodb-org-server hold" | sudo dpkg --set-selections
-echo "mongodb-org-shell hold" | sudo dpkg --set-selections
-echo "mongodb-org-mongos hold" | sudo dpkg --set-selections
-echo "mongodb-org-tools hold" | sudo dpkg --set-selections
-sudo service mongod start
-nohup mongod --dbpath bot_logic/data
+case $OSTYPE in
+  linux*)
+    if ! hash mongod 2>/dev/null; then
+      sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
+      echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
+      sudo apt-get update
+      sudo apt-get install -y mongodb-org
+      echo "mongodb-org hold" | sudo dpkg --set-selections
+      echo "mongodb-org-server hold" | sudo dpkg --set-selections
+      echo "mongodb-org-shell hold" | sudo dpkg --set-selections
+      echo "mongodb-org-mongos hold" | sudo dpkg --set-selections
+      echo "mongodb-org-tools hold" | sudo dpkg --set-selections
+    else
+        echo "Mongod already running"
+    fi
+    ;;
+  darwin*)
+    if ! hash mongod 2>/dev/null; then
+      brew update
+      brew install mongodb
+    else
+      echo "Mongod already running"
+    fi
+    ;;
+  bsd*)
+    echo "Installation steps missing for $OSTYPE, please add :-')"
+    ;;
+  solaris*)
+    echo "Installation steps missing for $OSTYPE, please add :-')"
+    ;;
+  *)
+  echo "Installation steps missing for $OSTYPE, please add :-')"
+  ;;
+esac
 chef gem install knife-block
